953aa209f1f51aaa340d45e541be481dab2679ea
Omri Gal Shenhav <Shenhav.omri@gmail.com>
2026-05-19 23:17:07 +0300

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res = []
+
+        def rec(i, l, s):
+            nonlocal res
+            if s == target:
+                res.append(l.copy())
+                return
+            if i >= len(nums) or s > target:
+                return
+            n = nums[i]
+            l.append(n)
+            rec(i, l, s+n)
+            l.pop()
+            rec(i+1, l, s)
+
+        rec(0, [], 0)
+        return res
