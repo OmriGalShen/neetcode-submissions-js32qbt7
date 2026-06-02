b4d90bd6512461de4646d514f067d51cbc79144b
Omri Gal Shenhav <Shenhav.omri@gmail.com>
2026-06-02 23:48:43 +0300

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+
+        def rec(i, l):
+            if len(l) == k:
+                res.append(l[:])
+                return
+            if i > n:
+                return
+            l.append(i)
+            rec(i+1, l)
+            l.pop()
+            rec(i+1, l)
+        
+        rec(1, [])
+        return res
