00d7ad6ed9d391b9ed2159ccee13863190e279bd
Omri Gal Shenhav <Shenhav.omri@gmail.com>
2026-06-07 23:21:43 +0300

    Add: permutations-ii - submission-0

---
+++ Data Structures & Algorithms/permutations-ii/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        visited = set()
+
+        def rec(l):
+            if len(l) == len(nums):
+                res.append(l[:])
+                return
+
+            for i in range(len(nums)):
+                if i in visited:
+                    continue
+                if i > 0 and nums[i] == nums[i-1] and i-1 not in visited:
+                    continue
+                visited.add(i)
+                l.append(nums[i])
+                rec(l)
+                visited.remove(i)
+                l.pop()
+        rec([])
+        return res
