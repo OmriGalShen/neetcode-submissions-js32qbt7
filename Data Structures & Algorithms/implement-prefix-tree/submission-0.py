fa99f5c47282d995703e9fc654818e38215d8e2f
Omri Gal Shenhav <Shenhav.omri@gmail.com>
2026-08-09 22:58:41 +0300

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, final) -> None:
+        self.childern = {}
+        self.final = final
+
+class PrefixTree:
+    def __init__(self):
+        self.head = Node(False)
+
+    def insert(self, word: str) -> None:
+        curr = self.head
+        for ch in word:
+            if ch not in curr.childern:
+                curr.childern[ch] = Node(False)
+            curr = curr.childern[ch]
+        curr.final = True
+
+    def search(self, word: str) -> bool:
+        curr = self.head
+        for ch in word:
+            if ch not in curr.childern:
+                return False
+            curr = curr.childern[ch]
+        return curr.final
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.head
+        for ch in prefix:
+            if ch not in curr.childern:
+                return False
+            curr = curr.childern[ch]
+        return True
