cb524eacd13df92223a66434292205a57293d0a8
Omri Gal Shenhav <Shenhav.omri@gmail.com>
2026-08-19 00:23:24 +0300

    Add: search-for-word-ii - submission-4

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-4.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self) -> None:
+        self.children = {}
+        self.word = ''
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        res = set()
+        visited = set()
+
+        head = Node()
+        N, M = len(board), len(board[0])
+
+        for word in words:
+            curr = head
+            for c in word:
+                if c not in curr.children:
+                    curr.children[c] = Node()
+                curr = curr.children[c]
+            curr.word = word
+
+        def dfs(row, col, node):
+            if row < 0 or col < 0 or row >= N or col >= M:
+                return
+            if (row, col) in visited:
+                return
+            c = board[row][col]
+            if c not in node.children:
+                return
+            visited.add((row,col))
+            node = node.children[c]
+            if node.word:
+                res.add(node.word)
+                node.word = ''
+            dfs(row +1, col, node)
+            dfs(row, col+1, node)
+            dfs(row -1, col, node)
+            dfs(row, col-1, node)
+            visited.remove((row,col))
+        
+        for row in range(N):
+            for col in range(M):
+                dfs(row,col, head)
+        return list(res)
+
+
+
+        
