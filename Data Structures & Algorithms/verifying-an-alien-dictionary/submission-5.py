6fcce847d2612df17fb5539ae7b8c669453e3781
Omri Gal Shenhav <Shenhav.omri@gmail.com>
2026-08-19 23:58:51 +0300

    Add: verifying-an-alien-dictionary - submission-5

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        d = { v: i for i, v in enumerate(order)}
+        for w1,w2 in zip(words,words[1:]):
+            for j in range(min(len(w1), len(w2))):
+                c = d[w1[j]] - d[w2[j]]
+                if c > 0:
+                    return False
+                elif c < 0:
+                    break
+            else:
+                if len(w1) > len(w2):
+                    return False
+        return True
+
