137d4a99504ca742e66282d1d7dffb27d403ae98
Omri Gal Shenhav <Shenhav.omri@gmail.com>
2026-08-29 22:34:14 +0300

    Add: kth-largest-element-in-an-array - submission-8

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-8.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for(int n: nums){
+            if(heap.size() < k){
+                heap.offer(n);
+            }else if (heap.peek() < n){
+                heap.poll();
+                heap.offer(n);
+            }
+        }
+        return heap.peek();
+    }
+}
