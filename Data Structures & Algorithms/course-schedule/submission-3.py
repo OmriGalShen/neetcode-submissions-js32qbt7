9afcf7481658716b11ed71c395214f7e62b52249
Omri Gal Shenhav <Shenhav.omri@gmail.com>
2026-09-21 01:00:10 +0300

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        m = defaultdict(list)
+        degrees = [0] * numCourses
+
+        for a,b in prerequisites:
+            m[b].append(a)
+            degrees[a] +=1
+        q = deque()
+        for i in range(numCourses):
+            if degrees[i] == 0:
+                q.append(i)
+        count = 0
+        while q:
+            i = q.popleft()
+            count += 1
+            courses = m[i]
+            for course in courses:
+                degrees[course] -= 1
+                if degrees[course] == 0:
+                    q.append(course)
+        
+        return count == numCourses
