26b23beffe6255c273c124895511ba6a4bd3d46c
Omri Gal Shenhav <Shenhav.omri@gmail.com>
2026-09-26 18:28:30 +0300

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int maxFreq = 0;
+        int[] counts = new int[26];
+        int l = 0;
+        int res = 0;
+        for (int r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+            counts[c - 'A']++;
+            maxFreq = Math.max(maxFreq, counts[c - 'A']);
+            if (r - l + 1 - maxFreq > k) {
+                counts[s.charAt(l) - 'A']--;
+                l++;
+            }
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
